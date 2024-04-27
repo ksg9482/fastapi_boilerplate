96a610fd4330274972f3fd4bbce5c86c8eca07f1
ksg9482 <ksg930523@gmail.com>
2024-04-27 18:48:49 +0900

    UPDATE: jwt

--- src/auth/jwt.py
+++ src/auth/jwt.py
@@ -3,7 +3,7 @@ from typing import Annotated
 
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-# from jose import JWTError, jwt
+from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
 
@@ -11,7 +11,12 @@ SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+"""
+타입 어노테이션 -> 변수, 함수인자, 반환 값의 기대되는 데이터 타입을 명시적으로 선언하는 방법
+파이썬 3.5에서 도입.
 
+Annotated: 유형에 상황별 메타데이터를 추가
+"""
 fake_users_db = {
     "johndoe": {
         "username": "johndoe",
@@ -33,7 +38,7 @@ class User(BaseModel):
     username: str
     email: str | None = None
     full_name: str | None = None
-    disabled: bool | None = None
+    disabled: bool | None = None # 유저 활성화 여부
 
 class UserInDB(User):
     hashed_password: str
@@ -84,6 +89,34 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     # 데이터 + 시그니처 검증용 비밀키 + 암호화 알고리즘
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+# oauth2_scheme는 의존성 주입을 가져옴 -> fastapi가 의존성 관리
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        # 헤더에 auth 넣음 -> Bearer까진 넣고 그 이후는 안넣음
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub") # sub는 토큰의 소유자를 나타냄
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if current_user.disabled: # 유저는 비활성화 되었지만 토큰은 유효할 수 있음. 특히 refresh_token 같은거.
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
 
 def create_jwt_token():
     return False
